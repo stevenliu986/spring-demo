5471f443e33ca196c1cdef795d8ad82723435770
StevenLiu <stevenliu986@gmail.com>
2022-12-05 20:17:45 +1100

    add comments on spring-ioc-annotation.xml

--- spring-ioc-annotation/src/test/java/com/example/spring/IOCByAnnotationTest.java
+++ spring-ioc-annotation/src/test/java/com/example/spring/IOCByAnnotationTest.java
@@ -1,6 +1,8 @@
 package com.example.spring;
 
 import com.example.spring.controller.UserController;
+import com.example.spring.dao.UserDao;
+import com.example.spring.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,5 +23,9 @@ public class IOCByAnnotationTest {
         ApplicationContext ioc = new ClassPathXmlApplicationContext("spring-ioc-annotation.xml");
         UserController userController = ioc.getBean(UserController.class);
         System.out.println(userController);
+        // UserService userService = ioc.getBean(UserService.class);
+        // System.out.println(userService);
+        // UserDao userDao = ioc.getBean(UserDao.class);
+        // userDao.saveUser();
     }
 }
